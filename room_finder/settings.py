e9952543e95364267f92c79a93d82d7a04f3623b
Binay Rai <106470939+shanks9876543210@users.noreply.github.com>
2024-07-08 21:51:29 +0545

    Update settings.py
    debug

--- room_finder/settings.py
+++ room_finder/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zfpq2)g1vmj(f!^js@wyt=o=c)qhsol9wm69@(ol1@wyesa6kw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
